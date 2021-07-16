61b06bbd235faa532c56516f86dc1ec952bd67b4
Mayur Machhi <mayurmachhi19@gmail.com>
2021-07-17 03:22:33 +0530

    live camera code removed(temporary)

--- qr-code-decoder_app.py
+++ qr-code-decoder_app.py
@@ -3,6 +3,7 @@
 import streamlit as st
 from PIL import Image
 import numpy as np
+from pyzbar.pyzbar import decode
 from functions import *
 #----------------------------------------------------------------------------------------------------------------------------
 
@@ -60,43 +61,44 @@ st.markdown(f"<p style='text-align: center; color: black;'>To generate a QR Code
 
 
 
-############################################################################################################################# 
-st.title("Webcam Live Feed")
-# run = st.checkbox('Run')
-FRAME_WINDOW = st.image([])
+# ############################################################################################################################# 
+# st.title("Webcam Live Feed")
+# # run = st.checkbox('Run')
+# FRAME_WINDOW = st.image([])
 
 
-def decoder(image):
-    gray_img = cv2.cvtColor(image,0)
-    barcode = decode(gray_img)
+# def decoder(image):
+#     gray_img = cv2.cvtColor(image,0)
+#     barcode = decode(gray_img)
 
-    for obj in barcode:
-        points = obj.polygon
-        (x,y,w,h) = obj.rect
-        pts = np.array(points, np.int32)
-        pts = pts.reshape((-1, 1, 2))
-        cv2.polylines(image, [pts], True, (0, 255, 0), 3)
+#     for obj in barcode:
+#         points = obj.polygon
+#         (x,y,w,h) = obj.rect
+#         pts = np.array(points, np.int32)
+#         pts = pts.reshape((-1, 1, 2))
+#         cv2.polylines(image, [pts], True, (0, 255, 0), 3)
 
-        barcodeData = obj.data.decode("utf-8")
-        barcodeType = obj.type
-        string = "Data: " + str(barcodeData) + " | Type: " + str(barcodeType)
+#         barcodeData = obj.data.decode("utf-8")
+#         barcodeType = obj.type
+#         string = "Data: " + str(barcodeData) + " | Type: " + str(barcodeType)
         
-        cv2.putText(frame, string, (x,y), cv2.FONT_HERSHEY_SIMPLEX,0.8,(0,0,255), 2)
-        print("Barcode: "+barcodeData +" | Type: "+barcodeType)
+#         cv2.putText(frame, string, (x,y), cv2.FONT_HERSHEY_SIMPLEX,0.8,(0,0,255), 2)
+#         print("Barcode: "+barcodeData +" | Type: "+barcodeType)
 
 
-run = st.button('Run')
-stop = st.button('Stop')
-st.write(run,stop)
-cap = cv2.VideoCapture(0)
-while run:
-    ret, frame = cap.read()
-    decoder(frame)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    FRAME_WINDOW.image(frame)
-else:
-    st.write('Stopped')
-#############################################################################################################################
+# run = st.button('Run')
+# stop = st.button('Stop')
+# st.write(run,stop)
+# cap = cv2.VideoCapture(0)
+# while run:
+#     ret, frame = cap.read()
+#     decoder(frame)
+#     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#     FRAME_WINDOW.image(frame)
+# else:
+#     st.write('Stopped')
+
+# #############################################################################################################################
 
 
 
